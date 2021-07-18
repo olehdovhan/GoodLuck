a2883ec373cc626339b9a594d139c3d4d200aa7b
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-07-18 21:00:38 +0300

    Add imageView and progressBar

---
+++ GoodLuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodLuck
+//
+//  Created by Oleh Dovhan on 18.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
