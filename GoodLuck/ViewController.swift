d55635098a037dc50e3ac2f5ec8c013e4afe73a9
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-07-18 23:41:13 +0300

    Set animation of progressBar, add WKWebview and Url

--- GoodLuck/ViewController.swift
+++ GoodLuck/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var webView: WKWebView! {
+    didSet {
+      self.webView.isHidden = true
+    }
+  }
+  
+  @IBOutlet weak var progressBar: UIProgressView!
+  
+  
+  @IBOutlet weak var readyButtonOutlet: UIButton! {
+    didSet {
+      self.readyButtonOutlet.isHidden = true
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    animateProgressBar()
+    DispatchQueue.main.asyncAfter(deadline: .now()+4) {
+      self.readyButtonOutlet.isHidden = false
+      guard let url = URL(string: "https://i.i-bbva.com/click.php?key=sv2n47z3n770fyx7tmjb&p=partner&c=creative&l=lander&off=offer&link=link" ) else { return }
+      let link = URLRequest(url: url)
+      self.webView.load(link)
+      print(self.webView.url)
+    }
+  }
+  
+  func animateProgressBar() {
+    for x in 0...100 {
+      DispatchQueue.main.asyncAfter(deadline: .now()+(Double(x)*0.03)) {
+        self.progressBar.setProgress(Float(x)/100, animated: true)
+      }
+    }
   }
 
-
+  @IBAction func readyButtonAction(_ sender: UIButton) {
+    
+    if self.webView.url == URL(string: "https://www.google.com/") {
+      self.readyButtonOutlet.isHidden = true
+      self.webView.isHidden = false
+      
+    } else {
+      
+    }
+   
+  }
+  
+  
 }
 
